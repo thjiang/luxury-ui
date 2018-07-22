c0ca5269d4a88efbbc7fbd8cadcd31d72bdcbef9
thjiang <browserify@qq.com>
2018-07-22 23:01:32 +0800

    :sparkles: feat: button

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,12 +1,41 @@
 <template>
-    <button>
-        <slot></slot>
+    <button class="lu-btn" :class="[
+            type ? `lu-btn--${type}` : '',
+            size ? `lu-btn--${size}` : '',
+            outline ? `lu-btn--${type}--outline` : ''
+        ]" :disabled="disabled" :type="nativeType" @click="handleClick">
+        <span v-if="showSlot" ref="slot">
+            <slot></slot>
+        </span>
     </button>
 </template>
 
 <script>
 export default {
-    name: "lu-button"
+    name: "lu-button",
+    props: {
+        type: {
+            type: String,
+            default: "default"
+        },
+        nativeType: {
+            type: String,
+            default: "button"
+        },
+        size: String,
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        outline: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        handleClick(e) {
+            this.$emit("click", e);
+        }
+    }
 };
 </script>
-
